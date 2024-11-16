11aed5dc656fc21da6c40dd679c3e8b600fee2dc
airplane9170 <57787104+airplane9170@users.noreply.github.com>
2024-11-16 23:23:15 +0900

    2

--- sns_message.py
+++ sns_message.py
@@ -1,14 +1,27 @@
 import datetime
 import requests
+
 # Discord
 def send_message(msg):
     now = datetime.datetime.now()
 
     DISCORD_WEBHOOK_URL =""
+   
+    embed = { 
+        "title": "Your Title", 
+        "description": f"{str(msg)}", 
+        "color": 5814783, # 색상은 10진수로 입력 (여기서는 초록색) 
+        "timestamp": now.isoformat(), 
+        "footer": { "text": "Your Footer Text" }, 
+        "fields": [ { "name": "Field1", "value": "Field1 Value", "inline": False }, { "name": "Field2", "value": "Field2 Value", "inline": True } ] } 
+    
+    message = { "username": "Your Bot Name", "embeds": [embed] } 
+    headers = { "Content-Type": "application/json" } 
+    response = requests.post(DISCORD_WEBHOOK_URL, json=message, headers=headers) 
 
-    message = {"content": f"**{str(msg)}**\n Date: {now.strftime('%Y-%m-%d %H:%M:%S')}"}
-    requests.post(DISCORD_WEBHOOK_URL, data=message)
-    print(message)
+    print(response.status_code,response.text)
+
+send_message("This is a test message")
 
 
 import telegram
